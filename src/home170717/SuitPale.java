fb768e6f41613d5f865b2f81de9844a5cdcaca9d
Julia Klueva <juliaklueva@yandex.ru>
2017-09-14 22:04:12 +0400

    Solitaire source

---
+++ src/home170717/SuitPale.java
@@ -0,0 +1,17 @@
+package home170717;
+
+class SuitPile extends CardPile {
+
+	SuitPile(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public boolean canTake(Card aCard) {
+		if (empty()) {
+			return aCard.getRank() == 0;
+		}
+		Card topCard = top();
+		return (aCard.getSuit() == topCard.getSuit()) && (aCard.getRank() == 1 + topCard.getRank());
+	}
+}
