912aa849e260d44a4efd63f1831bd0add26f8f5f
juliaklueva <juliaklueva@rambler.ru>
2017-09-15 06:05:55 +0300

    Update DecToHex.java

--- src/home170627/DecToHex.java
+++ src/home170627/DecToHex.java
@@ -2,13 +2,38 @@ package home170627;
 
 public class DecToHex {
 
-	public static void main(String[] args) {
+	public static String toHex(int decNumber) {
+		if (decNumber == 0) {
+			return "0";
+		}
+		String result = "";
+		int hexNumber;
+		char c = '\u0000';
+		while (decNumber != 0) {
+			hexNumber = decNumber % 16;
+			if (hexNumber > 9) {
+				if (hexNumber == 10) {
+					c = 'a'; 				}
+				if (hexNumber == 11) {
+					c = 'b'; 				}
+				if (hexNumber == 12) {
+					c = 'c'; 				}
+				if (hexNumber == 13) {
+					c = 'd'; 
+				if (hexNumber == 14) {
+					c = 'e'; 				}
+				if (hexNumber == 15) {
+					c = 'f'; 				}
+				result = c + result;
 
-		int dec = ConvertUtils.initDec();
+			} else {
+				result = hexNumber + result;
 
-		System.out.println("Dec = " + dec);
+			}
+			decNumber = decNumber / 16;
 
-		System.out.println("Hex = " +ConvertUtils.decToHexConverter(dec));
-
+		}
+		return result;
 	}
+
 }
