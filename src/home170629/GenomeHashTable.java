f3ea82a1df2298061b9e66d3ede22a233aca2591
Julia Klueva <juliaklueva@yandex.ru>
2017-09-15 04:47:11 +0400

    Genome HashTable

--- src/home170629/GenomeHashTable.java
+++ src/home170629/GenomeHashTable.java
@@ -1,5 +1,42 @@
 package home170629;
 
-public class GenomeHashMap {
-		//TODO
+import java.util.Hashtable;
+
+public class GenomeHashTable {
+	private static final int GENOME_SIZE = 300000;
+	private static final int WORD_SIZE = 4;
+
+	public static void wordFind(byte[] data) {
+
+		Hashtable<String, Integer> repeatedWords = new Hashtable<>();
+
+		for (int i = 0; i < data.length - WORD_SIZE + 1; i++) {
+			StringBuilder sb = new StringBuilder();
+
+			for (int j = 0; j < WORD_SIZE; j++) {
+				sb.append((char) data[i + j]);
+			}
+			String wordToFind = sb.toString();
+			if (repeatedWords.containsKey(wordToFind)) {
+				int count = repeatedWords.get(wordToFind);
+				count++;
+				repeatedWords.put(wordToFind, count);
+			} else {
+				repeatedWords.put(wordToFind, 1);
+			}
+		}
+
+		System.out.println(repeatedWords.size());
+
+		String[] array = repeatedWords.toString().split(", ");
+		for (String s : array) {
+			System.out.println(s);
+		}
+	}
+
+	public static void main(String[] args) {
+		byte[] data = GenomeGenerator.generate(GENOME_SIZE);
+
+		wordFind(data);
+	}
 }
